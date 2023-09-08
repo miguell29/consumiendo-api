cf07b2d7e785793a554b8cf5774df7d9aef76600
Miguel <mfjuaarez@gmail.com>
2023-09-07 22:02:07 -0300

    Se modifico la arquitectura de la app agregando los archivos y carpetas necesarios para la implementacion de interfaces y servicios

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,6 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #2c3e50;
 }
 </style>
